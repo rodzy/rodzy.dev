7b2e2adff5b016ee53efdfe73e16897211bc51df
rodzy <49137701+rodzy@users.noreply.github.com>
2020-10-05 23:33:45 -0600

    Updated viewports warnings

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,10 +9,6 @@ class MyDocument extends Document {
                         httpEquiv="Content-Type"
                         content="text/html; charset=utf-8"
                     />
-                    <meta
-                        name="viewport"
-                        content="width=device-width, initial-scale=1.0"
-                    />
                     {/* Twitter tags */}
                     <meta name="twitter:card" content="summary" />
                     <meta name="twitter:creator" content="@rodzyrm" />
