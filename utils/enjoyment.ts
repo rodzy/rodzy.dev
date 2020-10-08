67d382db38eda05f32cf2847df1db314cdbc4fc4
rodzy <49137701+rodzy@users.noreply.github.com>
2020-10-07 23:39:28 -0600

    Added enjoyments array

---
+++ utils/enjoyment.ts
@@ -0,0 +1,37 @@
+import { CurrentTypes } from "../types/CurrentTypes";
+
+export const enjoyment: CurrentTypes = [
+    {
+        id: 0,
+        name: "Next.js",
+        description:
+            "The React framework for production, there is no more introduction just the best of the best",
+        url: "/logos/nextjs-3 (1).svg",
+    },
+    {
+        id: 1,
+        name: "Gatsby.js",
+        description:
+            "Just for FUN🎉, Whenever I want to hook up some of the plugins with a headless CMS",
+        url: "/logos/gatsby.svg",
+    },
+    {
+        id: 2,
+        name: "TailwindCSS",
+        description: "It's just magical ✨",
+        url: "/logos/GraphQL_Logo.svg",
+    },
+    {
+        id: 3,
+        name: "Chakra UI",
+        description: "One of the greatest components libraries for React 👀",
+        url: "/logos/postgresql.svg",
+    },
+    {
+        id: 4,
+        name: "CSS modules",
+        description:
+            "A clean way to quickly get JS with CSS, without mixed globals 📦",
+        url: "/logos/postgresql.svg",
+    },
+];
