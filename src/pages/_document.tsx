b214ac5ee6e21a52ba7a6e963d186a16732ae1a3
rodzy <49137701+rodzy@users.noreply.github.com>
2020-10-09 12:01:44 -0600

    [Fix] Typo @ _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,14 +14,14 @@ class MyDocument extends Document {
                     <meta name="twitter:creator" content="@rodzyrm" />
                     <meta
                         name="twitter:image"
-                        content="/me/rodzy-humble.webp"
+                        content="/me/rodzy-humble-2.webp"
                     />
                     {/*Open graph tags */}
                     <meta name="og:url" content="https://rodzy.vercel.app" />
                     <meta name="og:site_name" content="Isaac Rodríguez" />
                     <meta
                         property="og:image"
-                        content="/me/rodzy-humble.webp"
+                        content="/me/rodzy-humble-2.webp"
                     />
                     <link rel="icon" href="/favicon.ico" />
                 </Head>
